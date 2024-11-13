850b64ccbcc92fbc2b89be899cd9e7610320528c
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2024-11-14 01:25:26 +0530

    Pattern Problems (Example: Pyramid Pattern)

---
+++ Java Code/Pattern.java
@@ -0,0 +1,17 @@
+public class Pattern {
+    public static void printPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printPyramid(5);
+    }
+}
